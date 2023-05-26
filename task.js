cf1aaf34004fdd4668ed29890e64391d652cfcea
litvinovjobs@gmail.com <litvinovjobs@gmail.com>
2023-05-26 21:53:12 +0300

    codding teaching Class in JS

--- task.js
+++ task.js
@@ -1,10 +1,33 @@
 "use strict"
 
-const progressDone = document.querySelectorAll('.progress-done');
- 
-progressDone.forEach(progress => {
-	progress.style.width = progress.getAttribute('data-done') + '%';
-});
+class User {
+	
+	constructor (username, password) {
+		this.username = username;
+		this.password = password;
+	}
+	validatePassword() {
+		// > 6
+		if (this.password.length > 6) {
+			return true;
+		}
+		return false;
+	}
+
+}
+
+
+let a = 'Ivan';
+let b = '777';
+
+const person = new User(a, b);
+console.log("person:", person)
+console.log("person.username:", person.username)
+console.log("person.validatePassword()", person.validatePassword());
+
+
+
+
 
 
 
